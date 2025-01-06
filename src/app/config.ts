eb5031ced08af258566a3c4c86e68acb1e83f7b3
ironreplica <trevorchilds8@gmail.com>
2025-01-05 19:46:05 -0700

    removed useless files

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,36 +1,36 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
-import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp, getApps } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getDatabase } from "firebase/database";
+// import { getStorage } from "firebase/storage";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_UR,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_UR,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+// };
 
-// Initialize Firebase
-// export default function config(){
-//   if (getApps().length === 0){
-//     initializeApp(firebaseConfig)
+// // Initialize Firebase
+// // export default function config(){
+// //   if (getApps().length === 0){
+// //     initializeApp(firebaseConfig)
 
-//     if(typeof window !== 'undefined')
-//   }
-// }
-const firebase_app =
-  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
-const database = getDatabase(firebase_app);
-const auth = getAuth(firebase_app);
+// //     if(typeof window !== 'undefined')
+// //   }
+// // }
+// const firebase_app =
+//   getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+// const database = getDatabase(firebase_app);
+// const auth = getAuth(firebase_app);
 
-export default firebase_app;
-export const storage = getStorage(firebase_app);
-export { database };
-export { auth };
+// export default firebase_app;
+// export const storage = getStorage(firebase_app);
+// export { database };
+// export { auth };
--- src/middleware/get-api-ai.test.js
+++
@@ -1,6 +0,0 @@
-// import { GetApiAi } from "./get-api-ai";
-// describe("Get-Api-Ai", () => {
-//   it("should return a 200 OK response from the ai api", async () => {
-//     const responseCode = await GetApiAi();
-//   });
-// });
