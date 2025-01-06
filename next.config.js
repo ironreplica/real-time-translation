8185a9fff754247cb8c6fb4e58a72d7f13bfd2f0
ironreplica <trevorchilds8@gmail.com>
2025-01-05 23:12:46 -0700

    added-changes-netlify

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
+  // Remove swcMinify if it's causing issues
+  // swcMinify: true,
   // Add any other necessary configuration options here
 };
